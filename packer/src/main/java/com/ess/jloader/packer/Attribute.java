50ca112f4a7518783305acf3949c12c2fb99e6fb
Sergey Evdokimov <sergey.evdokimov@jetbrains.com>
2014-08-04 10:49:07 +0400

    Add NotNull

--- packer/src/main/java/com/ess/jloader/packer/Attribute.java
+++ packer/src/main/java/com/ess/jloader/packer/Attribute.java
@@ -1,5 +1,7 @@
 package com.ess.jloader.packer;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -11,13 +13,20 @@ public abstract class Attribute {
 
     private String name;
 
-    public Attribute(String name) {
+    public Attribute(@NotNull String name) {
         this.name = name;
     }
 
     public abstract void writeTo(DataOutputStream out, ClassDescriptor descriptor) throws IOException;
 
+    @NotNull
     public String getName() {
         return name;
     }
+
+    @Override
+    public String toString() {
+        return getName();
+    }
+
 }
