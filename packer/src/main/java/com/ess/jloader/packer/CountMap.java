3f4927a851541bd1742344ca8f1cb6c6c553c5b7
Sergey Evdokimov <sergey.evdokimov@jetbrains.com>
2014-08-23 00:50:03 +0400

    Add CountMap.asOrderedMap() method

--- packer/src/main/java/com/ess/jloader/packer/CountMap.java
+++ packer/src/main/java/com/ess/jloader/packer/CountMap.java
@@ -2,9 +2,7 @@ package com.ess.jloader.packer;
 
 import org.jetbrains.annotations.Nullable;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -53,6 +51,23 @@ public class CountMap<T> {
 
     public AtomicInteger putCounter(T key, AtomicInteger counter) {
         return map.put(key, counter);
+    }
+
+    public Map<T, Integer> asOrderedMap() {
+        List<T> keys = new ArrayList<T>(map.keySet());
+        Collections.sort(keys, new Comparator<T>() {
+            @Override
+            public int compare(T o1, T o2) {
+                return Integer.compare(map.get(o2).get(), map.get(o1).get());
+            }
+        });
+
+        Map<T, Integer> res = new LinkedHashMap<T, Integer>();
+        for (T key : keys) {
+            res.put(key, map.get(key).get());
+        }
+
+        return res;
     }
 
     public int get(T key) {
